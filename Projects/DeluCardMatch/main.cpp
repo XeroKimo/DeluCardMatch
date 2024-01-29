23612c5db1daac4fbfde7006c4b927fcc9322b5f
Renzy Alarcon <Renzy.Alarcon@hotmail.com>
2024-01-28 20:15:39 -0500

    - Removed anchor based drawing as pin + relative size handles that case, will eventually add a helper function that will calculate pin + relative size based on anchors - Pin + relative size and pin + static size seems to be working properly - Added a GetRect function that will properly calculate the position + size of a ui element regardless of how it's being represented

--- Projects/DeluCardMatch/main.cpp
+++ Projects/DeluCardMatch/main.cpp
@@ -12,6 +12,7 @@
 #include <box2d/box2d.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
+#include <variant>
 
 import DeluEngine;
 import xk.Math.Matrix;
@@ -45,21 +46,68 @@ struct UIFrame
 
 struct UIElement
 {
-	Vector2 position;
-	Vector2 size;
-	Vector2 pivot;
-	SDL2pp::unique_ptr<SDL2pp::Texture> texture;
-
-	SDL2pp::FRect UIRect() const
+	struct PinStaticSize
 	{
-		SDL2pp::FRect rect;
-		rect.x = pivot.X() * -size.X() + position.X();
-		rect.y = (1 - pivot.Y()) * -size.Y() - position.Y();
-		rect.w = size.X();
-		rect.h = size.Y();
-		return rect;
-	}
+		Vector2 pin;
+		Vector2 size;
+	};
+
+	struct PinRelativeSize
+	{
+		Vector2 pin;
+		Vector2 size;
+	};
+
+	using PositionSizeVariant = std::variant<PinStaticSize, PinRelativeSize>;
+
+	PositionSizeVariant positionSize;
+	Vector2 pivot;
+	SDL2pp::shared_ptr<SDL2pp::Texture> texture;
+
 };
+
+SDL2pp::FRect GetRect(const UIFrame& frame, const UIElement& element)
+{
+	SDL2pp::FRect rect;
+	const Vector2 size = [&frame, &element]() -> Vector2
+	{
+		if(auto* pinStaticSize = std::get_if<UIElement::PinStaticSize>(&element.positionSize); pinStaticSize)
+		{
+			return pinStaticSize->size;
+		}
+		else if(auto* pinRelativeSize = std::get_if<UIElement::PinRelativeSize>(&element.positionSize); pinRelativeSize)
+		{
+			return { frame.size.X() * pinRelativeSize->size.X(), frame.size.Y() * pinRelativeSize->size.Y() };
+		}
+		else
+		{
+			throw std::runtime_error("Unhandled case\n");
+		}
+	}();
+	const Vector2 pivotPositionOffset = { element.pivot.X() * -size.X(), (1 - element.pivot.Y()) * -size.Y() };
+	const Vector2 sdl2YFlip = { 0, frame.size.Y() };
+	const Vector2 position = [&frame, &element] () -> Vector2
+	{
+			if(auto* pinStaticSize = std::get_if<UIElement::PinStaticSize>(&element.positionSize); pinStaticSize)
+			{
+				return { frame.size.X() * pinStaticSize->pin.X(), frame.size.Y() * -pinStaticSize->pin.Y() };
+			}
+			else if(auto* pinRelativeSize = std::get_if<UIElement::PinRelativeSize>(&element.positionSize); pinRelativeSize)
+			{
+				return { frame.size.X() * pinRelativeSize->pin.X(), frame.size.Y() * -pinRelativeSize->pin.Y() };
+			}
+			else
+			{
+				throw std::runtime_error("Unhandled case\n");
+			}
+	}() + pivotPositionOffset + sdl2YFlip;
+
+	rect.x = position.X();
+	rect.y = position.Y();
+	rect.w = size.X();
+	rect.h = size.Y();
+	return rect;
+}
 
 int main()
 {
@@ -82,7 +130,7 @@ int main()
 		});
 	std::chrono::duration<float> physicsAccumulator{0.f};
 
-	UIFrame frame{ { 1280, 720 } };
+	UIFrame frame{ { 1600, 900 } };
 	frame.internalTexture = engine.renderer.backend->CreateTexture(
 		SDL_PIXELFORMAT_RGBA32, 
 		SDL2pp::TextureAccess(SDL_TEXTUREACCESS_STATIC | SDL_TEXTUREACCESS_TARGET), 
@@ -96,9 +144,19 @@ int main()
 	SDL_Texture* testFontTexture = SDL_CreateTextureFromSurface(engine.renderer.backend.get(), testFontSurface);
 	SDL_Surface* testSurface = IMG_Load("Cards/syobontaya.png");
 
-	UIElement testElement;
-	testElement.texture = engine.renderer.backend->CreateTexture(testSurface);
-	testElement.size = testElement.texture->GetSize();
+	UIElement testElement
+	{
+		UIElement::PinRelativeSize{ .pin ={0.2f, 0.2f}, .size = { 0.2f, 0.2f } },
+		{ 1.f, 1.f},
+		engine.renderer.backend->CreateTexture(testSurface)
+	};
+	//testElement.positionSize = ;
+	//testElement.texture = std::move(engine.renderer.backend->CreateTexture(testSurface));
+
+	UIElement testElement2 = testElement;
+	testElement2.positionSize = UIElement::PinRelativeSize{ .pin = {0.5f, 0.5f}, .size = { 2.f, 2.f } };
+	testElement2.pivot = { 0.5f, 0.5f };
+	//testElement.size = testElement.texture->GetSize();
 	//testElement.pivot.Y() = 1;
 	//testElement.position.Y() = testElement.texture->GetSize().Y();
 	std::chrono::duration<float> accumulator{ 0 };
@@ -120,7 +178,7 @@ int main()
 			timer.Tick([&](std::chrono::nanoseconds dt)
 			{
 				//testElement.position.Y() = testElement.size.Y() * std::sin(accumulator.count());
-				//testElement.pivot.Y() = (std::sin(accumulator.count()) + 1) / 2;
+				testElement.pivot.Y() = (std::sin(accumulator.count()));
 				std::cout << testElement.pivot.Y() << "\n";
 				std::chrono::duration<float> deltaTime = std::chrono::duration_cast<std::chrono::duration<float>>(dt);
 				static constexpr std::chrono::duration<float> physicsStep{ 1.f / 60.f };
@@ -146,10 +204,14 @@ int main()
 				engine.renderer.backend->Clear();
 				SDL_Rect textLocation = { 400, 200, testFontSurface->w, testFontSurface->h };
 				engine.renderer.backend->Copy(testFontTexture, std::nullopt, textLocation);
-				auto rect = testElement.UIRect();
-				rect.y += engine.renderer.backend->GetOutputSize().Y();
-				std::cout << rect.y << "\n";
+				//auto rect = testElement.UIRect();
+				//rect.x += engine.renderer.backend->GetOutputSize().X() * testElement.pin.X();
+				//rect.y -= engine.renderer.backend->GetOutputSize().Y() * testElement.pin.Y();
+				//rect.y += engine.renderer.backend->GetOutputSize().Y();
+
+				auto rect = GetRect(frame, testElement);
 				engine.renderer.backend->CopyEx(testElement.texture.get(), std::nullopt, rect, 0, SDL2pp::FPoint{ 0, 0 }, SDL2pp::RendererFlip::SDL_FLIP_NONE);
+				engine.renderer.backend->CopyEx(testElement.texture.get(), std::nullopt, GetRect(frame, testElement2), 0, SDL2pp::FPoint{ 0, 0 }, SDL2pp::RendererFlip::SDL_FLIP_NONE);
 
 				engine.renderer.backend->SetRenderTarget(nullptr);
 				engine.renderer.backend->Copy(frame.internalTexture.get(), std::nullopt, std::optional<SDL2pp::Rect>(std::nullopt));
